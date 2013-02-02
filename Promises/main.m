70b8b90ab603304db031b5557acf999ca65517d2
Jim Kubicek <jim.kubicek@smule.com>
2013-02-01 21:24:30 -0800

    Initial Commit

---
+++ Promises/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Promises
+//
+//  Created by Jim Kubicek on 2/1/13.
+//  Copyright (c) 2013 Jim Kubicek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

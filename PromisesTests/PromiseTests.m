d0a66e81076b95818550a056b40367b478fe9a3a
Jim Kubicek <jim.kubicek@smule.com>
2013-05-12 00:06:08 -0700

    Started adding promise tests

---
+++ PromisesTests/PromiseTests.m
@@ -0,0 +1,36 @@
+//
+//  PromiseTests.m
+//  Promises
+//
+//  Created by Jim Kubicek on 5/12/13.
+//  Copyright (c) 2013 Jim Kubicek. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PromiseTests : SenTestCase
+
+@end
+
+@implementation PromiseTests
+
+#pragma mark - Setup / Teardown
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+#pragma mark - Tests
+
+
+@end
